88e2ec2ca1a1d631e5eaae56bb9d894bc9b6bb00
Руслан Завьялов <ruslan70797@gmail.com>
2023-05-22 22:54:21 +0300

    ноутбук

---
+++ Laptop.java
@@ -0,0 +1,7 @@
+public class Laptop {
+    public String ID;
+    public Integer RAM; //4 8 16 32 64
+    public  Integer HDD;// жесткий диск гб - тб ?
+    public String OS;
+    public String colour;
+}
